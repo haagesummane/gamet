d2a49cdd6d96d0969b990a2b4f71d97e04955f25
manu-spherical <manuhegdev@gmail.com>
2018-08-30 19:28:23 +0530

    team colour

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,121 +4,127 @@ class About extends Component {
   render() {
     return (
       <AppBody active={this.props.active}>
-        <div className="contbcg" style={{ width: '80%', textAlign: 'center' }}>
-          {/* <h2>Welcome to Gamet seeds</h2>
-          <img src={logo} style={{ height: '260px' }} alt="Gamet seeds" /> */}
-          <h1>About Us</h1>
-          <hr />
-          <article style={{ textAlign: 'justify' }}>
-            <p>
-              Seed is the most important and critical input component that ensures productivity in
-              crop cultivation. Agriculture sector is highly dependent on the availability and
-              quality of seeds. Food security and sustainable agriculture production of any country
-              demands a systematic, strong and vibrant seed production system. We are witnessing a
-              rapid growth in hybridization and the demand for quality seed is increasing throughout
-              the globe. However, the challenge lies in meeting the expected quality, cost and
-              timely delivery.
-            </p>
-            <p>
-              In India, Agriculture is the dominant occupation, which secures abundant opportunities
-              for the seed research, seed production and marketing domains .From a predominantly
-              public sector in the 1960's, the Indian seed sector has evolved gradually in the last
-              40 years into a multi-faceted industry with involvement of both the public sector
-              organization and a large number of private firms, including some prominent global seed
-              players. India has emerged as one of the five largest seed markets across the globe.
-              Inherent strengths of India are, being the 2nd largest arable land with 46 soil types,
-              across 15 agro-climatic zones. This favors the seed production of diverse crops and
-              ensures continuous seed production activity throughout the year. This makes India, an
-              avenue for seed production hub since the country has mutual advantages of trade, due
-              to low transaction cost, availability of skilled human resource and scope for faster
-              and delivery. Besides this, enabling policy support from government such as tax
-              exemption, credit on soft terms, duty free import of machinery and integrated approach
-              towards seed science research, work as growth drivers for Indian seed sector.
-            </p>
-            <p>
-              “Gamet Seeds “is a group of experienced and committed seed professionals who are
-              engaged in quality seed manufacturing and supply for field (row) crops, vegetable
-              crops and fodder crops. Gamet Seeds is based and operates from Bangalore (India).
-              Gamet Seeds has a large network of experienced out growers and trained &skilled human
-              resource in different seed production zones of India. Gamet has developed seed
-              production system for each crop which is specific to different seasons and locations
-              across India. Our seed production system includes Production planning, Agronomic
-              practices and Total quality management system throughout the crop cycle. The
-              stakeholders; such as out growers ,field management staff in the Production ecosystem
-              get continuous knowledge and trainings on the seed production technology aspects;
-              particularly on male and female staggering, effective pollination, isolation
-              maintenance, off types rouging, yield estimation and post harvest management. This
-              enables us to achieve planned quantity with right quality (Genetic, Physiological and
-              Physical) and importantly timely supplies.
-            </p>
-            <div>
-              <h3>Gamet Seeds Focus Crops:</h3> Following are the focus crops of Gamet for the
-              quality seed supply both for domestic (Indian) and international clients:
-              <br />
-              <table
-                style={{ width: '100%', padding: '10px' }}
-                className="table-dark table-striped"
-              >
-                <thead>
-                  <tr>
-                    <th>Field Crops</th>
-                    <th>Vegetable Crops</th>
-                    <th>Fodder Crops</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Corn</td>
-                    <td>Tomato</td>
-                    <td>SSG (Fodder Sorghum)</td>
-                  </tr>
-                  <tr>
-                    <td>Cotton</td>
-                    <td>Hot pepper (Chilli)</td>
-                  </tr>
-                  <tr>
-                    <td>Sorghum</td>
-                    <td>Squash</td>
-                  </tr>
-                  <tr>
-                    <td>Pearl millet (Bajra)</td>
-                    <td>Water melon</td>
-                  </tr>
-                </tbody>
-              </table>
-              <br />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 contbcg1" style={{ width: '80%', textAlign: 'center' }}>
+              <h1>About Us</h1>
+              <hr />
+              <article style={{ textAlign: 'justify' }}>
+                <p>
+                  Seed is the most important and critical input component that ensures productivity
+                  in crop cultivation. Agriculture sector is highly dependent on the availability
+                  and quality of seeds. Food security and sustainable agriculture production of any
+                  country demands a systematic, strong and vibrant seed production system. We are
+                  witnessing a rapid growth in hybridization and the demand for quality seed is
+                  increasing throughout the globe. However, the challenge lies in meeting the
+                  expected quality, cost and timely delivery.
+                </p>
+                <p>
+                  In India, Agriculture is the dominant occupation, which secures abundant
+                  opportunities for the seed research, seed production and marketing domains .From a
+                  predominantly public sector in the 1960's, the Indian seed sector has evolved
+                  gradually in the last 40 years into a multi-faceted industry with involvement of
+                  both the public sector organization and a large number of private firms, including
+                  some prominent global seed players. India has emerged as one of the five largest
+                  seed markets across the globe. Inherent strengths of India are, being the 2nd
+                  largest arable land with 46 soil types, across 15 agro-climatic zones. This favors
+                  the seed production of diverse crops and ensures continuous seed production
+                  activity throughout the year. This makes India, an avenue for seed production hub
+                  since the country has mutual advantages of trade, due to low transaction cost,
+                  availability of skilled human resource and scope for faster and delivery. Besides
+                  this, enabling policy support from government such as tax exemption, credit on
+                  soft terms, duty free import of machinery and integrated approach towards seed
+                  science research, work as growth drivers for Indian seed sector.
+                </p>
+                <p>
+                  “Gamet Seeds “is a group of experienced and committed seed professionals who are
+                  engaged in quality seed manufacturing and supply for field (row) crops, vegetable
+                  crops and fodder crops. Gamet Seeds is based and operates from Bangalore (India).
+                  Gamet Seeds has a large network of experienced out growers and trained &skilled
+                  human resource in different seed production zones of India. Gamet has developed
+                  seed production system for each crop which is specific to different seasons and
+                  locations across India. Our seed production system includes Production planning,
+                  Agronomic practices and Total quality management system throughout the crop cycle.
+                  The stakeholders; such as out growers ,field management staff in the Production
+                  ecosystem get continuous knowledge and trainings on the seed production technology
+                  aspects; particularly on male and female staggering, effective pollination,
+                  isolation maintenance, off types rouging, yield estimation and post harvest
+                  management. This enables us to achieve planned quantity with right quality
+                  (Genetic, Physiological and Physical) and importantly timely supplies.
+                </p>
+                <div>
+                  <h3>Gamet Seeds Focus Crops:</h3> Following are the focus crops of Gamet for the
+                  quality seed supply both for domestic (Indian) and international clients:
+                  <br />
+                  <table
+                    style={{ width: '100%', padding: '10px' }}
+                    className="table-dark table-striped"
+                  >
+                    <thead>
+                      <tr>
+                        <th>Field Crops</th>
+                        <th>Vegetable Crops</th>
+                        <th>Fodder Crops</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Corn</td>
+                        <td>Tomato</td>
+                        <td>SSG (Fodder Sorghum)</td>
+                      </tr>
+                      <tr>
+                        <td>Cotton</td>
+                        <td>Hot pepper (Chilli)</td>
+                      </tr>
+                      <tr>
+                        <td>Sorghum</td>
+                        <td>Squash</td>
+                      </tr>
+                      <tr>
+                        <td>Pearl millet (Bajra)</td>
+                        <td>Water melon</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <br />
+                </div>
+                <div>
+                  <h3>Gamet Seeds Activities</h3>
+                  <ul>
+                    <li> Foundation /Basic seed multiplication and supply</li>
+                    <li> Hybrid seed production research </li>
+                    <li> Hybrid seed productionand supply</li>
+                  </ul>
+                </div>
+                <div>
+                  <h3>Why Gamet Seeds?</h3>
+                  <ul>
+                    <li>
+                      Frontline Seed companies (client) can outsource the above activities on
+                      turnkey basis to Gamet.
+                    </li>
+                    <li>
+                      Gamet helps in quicker scale up of client's seed business with minimum
+                      overheads.
+                    </li>
+                    <li>
+                      Our clients have the privilege of obtaining the traceability of on field
+                      production process,  on real time basis leveraging the digital technology and
+                      platforms.
+                    </li>
+                    <li>
+                      While achieving the business goals; Gamet links our clients to thousands of
+                      privileged farmers as out growers, who gets indirect employment through
+                      Gamet’s seed multiplication activities
+                    </li>
+                  </ul>
+                </div>
+              </article>
             </div>
-            <div>
-              <h3>Gamet Seeds Activities</h3>
-              <ul>
-                <li> Foundation /Basic seed multiplication and supply</li>
-                <li> Hybrid seed production research </li>
-                <li> Hybrid seed productionand supply</li>
-              </ul>
-            </div>
-            <div>
-              <h3>Why Gamet Seeds?</h3>
-              <ul>
-                <li>
-                  Frontline Seed companies (client) can outsource the above activities on turnkey
-                  basis to Gamet.
-                </li>
-                <li>
-                  Gamet helps in quicker scale up of client's seed business with minimum overheads.
-                </li>
-                <li>
-                  Our clients have the privilege of obtaining the traceability of on field
-                  production process,  on real time basis leveraging the digital technology and
-                  platforms.
-                </li>
-                <li>
-                  While achieving the business goals; Gamet links our clients to thousands of
-                  privileged farmers as out growers, who gets indirect employment through Gamet’s
-                  seed multiplication activities
-                </li>
-              </ul>
-            </div>
-            <div>
+          </div>
+          <div className="row">
+            <div className="col-12 contbcg2">
               <h1 style={{ textAlign: 'center', width: '100%' }}>Our Team</h1>
               <hr />
               <article style={{ textAlign: 'justify' }}>
@@ -130,7 +136,7 @@ class About extends Component {
                     was with Cargill Seeds and Monsanto India for 18 years in senior leadership role
                     for Seed Quality Assurance, Seed Production Research, Pre-foundation &Foundation
                     seed, Hybrid seed production and Seed exports. As a management savvy Seed Tech,
-                    he is passionate and hungry to explore more for the cause of Seed Industry.{' '}
+                    he is passionate and hungry to explore more for the cause of Seed Industry.
                     <br />Email:
                   </p>
                 </div>
@@ -165,7 +171,7 @@ class About extends Component {
                 </div>
               </article>
             </div>
-          </article>
+          </div>
         </div>
       </AppBody>
     );
